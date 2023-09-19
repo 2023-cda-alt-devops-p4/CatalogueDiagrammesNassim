ecc53af5906c0864e2d1fc738fa27280fdafa64c
Deflandrenassim <nassim.def@gmail.com>
2023-09-19 11:43:42 +0200

    feature: add card Components
    J'ai crée mon composant Card

---
+++ uml/src/components/Card/Card.jsx
@@ -0,0 +1,30 @@
+import Image from 'next/image'
+
+export const Card = ({ children }) => {
+    return (
+        <div>
+            {children}
+        </div>
+    )
+}
+
+export const CardPicture = ({ children, src, alt }) => {
+    return (
+        <>
+            <Image
+                src={src}
+                width={500}
+                height={500}
+                alt={alt}
+            />
+            {children}
+        </>
+    )
+}
+export const CardTitle = ({ children }) => {
+    return (
+        <div>
+            {children}
+        </div>
+    )
+}
