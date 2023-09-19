dca7f05c9e3e03f3997e833a6088b2c006b6ebbf
Deflandrenassim <nassim.def@gmail.com>
2023-09-19 13:49:55 +0200

    feat : ajout de ma data diagram
    ajout de données de mon api interne

--- uml/src/data/Diagram.jsx
+++ uml/src/data/Diagram.jsx
@@ -1,51 +1,104 @@
+// groupe 1 = Diagrammes de Modelisation Structurel
+// groupe 2 = Diagrammes de Modelisation Comportementaux
+
 const UmlData = [
     {
         id: 1,
+        groupe: 1,
         name: "Diagrammes de Paquetage.",
         picture: "fkzdfervg",
         description: "loremIpsum"
     },
     {
         id: 2,
-        name: "Diagrammes Structurels."
+        groupe: 1,
+        name: "Diagrammes Structurels.",
+        picture: "fkzdfervg",
+        description: "loremIpsum"
     },
     {
         id: 3,
+        groupe: 1,
         name: "Diagrammes d'Objets.",
         picture: "fkzdfervg",
         description: "loremIpsum"
     },
     {
         id: 4,
+        groupe: 1,
         name: "Diagrammes de Structure Composites.",
         picture: "fkzdfervg",
         description: "loremIpsum"
     },
     {
         id: 5,
-        name: "Diagrammes de Composants.",
+        groupe: 1,
+        name: "Diagrammes de Composants.",
         picture: "fkzdfervg",
         description: "loremIpsum"
     },
     {
         id: 6,
-        name: "Diagrammes de Déploiement",
+        groupe: 1,
+        name: "Diagrammes de Déploiement",
         picture: "fkzdfervg",
         description: "loremIpsum"
     },
     {
         id: 7,
-        name: "Diagrammes de Cas d'Utilisation.",
+        groupe: 2,
+        name: "Diagrammes de cas d'Utilisation.",
         picture: "fkzdfervg",
         description: "loremIpsum"
     },
     {
         id: 8,
-        name: " Diagrammes de Gant",
+        groupe: 2,
+        name: " Diagrammes d'activité",
         picture: "fkzdfervg",
         description: "loremIpsum"
     },
-
+    {
+        id: 9,
+        groupe: 2,
+        name: " Diagrammes de StateMachine",
+        picture: "fkzdfervg",
+        description: "loremIpsum"
+    },
+    {
+        id: 10,
+        groupe: 2,
+        name: " Diagrammes de communication",
+        picture: "fkzdfervg",
+        description: "loremIpsum"
+    },
+    {
+        id: 11,
+        groupe: 2,
+        name: " Diagrammes de séquence",
+        picture: "fkzdfervg",
+        description: "loremIpsum"
+    },
+    {
+        id: 12,
+        groupe: 2,
+        name: " Diagrammes de temps",
+        picture: "fkzdfervg",
+        description: "loremIpsum"
+    },
+    {
+        id: 13,
+        groupe: 2,
+        name: " Diagrammes d'apercu d'interaction",
+        picture: "fkzdfervg",
+        description: "loremIpsum"
+    },
+    {
+        id: 14,
+        name: " Diagrammes de Vue d'ensemble d'interaction",
+        picture: "fkzdfervg",
+        description: "loremIpsum"
+    }
 
 
 
