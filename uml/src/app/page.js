286795d7161c28659415487c83c111b101d77d77
Deflandrenassim <nassim.def@gmail.com>
2023-09-20 12:07:52 +0200

    feature: add component header and logo site

--- uml/src/app/page.js
+++ uml/src/app/page.js
@@ -1,9 +1,10 @@
 import { Umls } from "@/containers/Umls/Umls"
-
+import Header from "../components/Header"
 
 export default function Home() {
   return (
     <main>
+      <Header />
       <div> Hello World le Diagram : </div>
       <Umls />
     </main>
