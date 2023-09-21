ef8542313e7cd775e28df5457c0b64dabaae3b63
Deflandrenassim <nassim.def@gmail.com>
2023-09-21 12:36:53 +0200

    feat: resize card

--- uml/src/app/page.js
+++ uml/src/app/page.js
@@ -5,8 +5,9 @@ export default function Home() {
   return (
     <main>
       <HeaderContainer />
-      <div className="page_border"></div>
-      <Umls />
+      <div className="page_border">
+        <Umls />
+      </div>
     </main>
   )
 }
