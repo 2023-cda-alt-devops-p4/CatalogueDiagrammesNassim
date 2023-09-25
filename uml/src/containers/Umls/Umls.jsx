e451bbc2f22012b566bef60648ed2530aba5f395
Deflandrenassim <nassim.def@gmail.com>
2023-09-25 11:38:29 +0200

    feat : create modal component

--- uml/src/containers/Umls/Umls.jsx
+++ uml/src/containers/Umls/Umls.jsx
@@ -1,11 +1,18 @@
 'use client'
 import './Umls.css';
+import { useState } from 'react';
 import { umlData } from "@/data/Diagram";
 import { CardTitle, Card, CardPicture, CardPosition } from "@/components/Card/Card";
 import { Button } from "@/components/button/Button";
 
 export const Umls = () => {
-    console.table("all", umlData)
+    const [displayModal, setDisplayModal] = useState(false);
+
+
+    const handleModal = () => {
+        setDisplayModal(!displayModal)
+        console.log("displayModal", displayModal)
+    }
     return (
         <div className="umls_border">
             <div className="umls_container">
@@ -14,7 +21,7 @@ export const Umls = () => {
                         <CardPicture src={item.picture} alt={item.name} />
                         <CardPosition center="center">
                             <CardTitle>  {item.name}</CardTitle>
-                            <Button variant="variant" size="sizeM"> Plus d'info</Button>
+                            <Button onClick={() => handleModal()} variant="variant" size="sizeM"> Plus d'info</Button>
                         </CardPosition>
                     </Card>
                 ))}
