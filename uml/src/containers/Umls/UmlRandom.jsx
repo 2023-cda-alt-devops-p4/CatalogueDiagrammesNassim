58eeff42878cd18bee5eba534223eb792652f197
Deflandrenassim <nassim.def@gmail.com>
2023-09-27 14:16:36 +0200

    feat : create css btn random

--- uml/src/containers/Umls/UmlRandom.jsx
+++ uml/src/containers/Umls/UmlRandom.jsx
@@ -1,10 +1,12 @@
 "use client";
+import './Umls.css';
 import { useLayoutEffect, useState } from "react";
 import { umlData } from "@/data/Diagram";
 import { Button } from "@/components/button/Button";
-import { Card, CardDescription, CardPicture } from "@/components/Card/Card";
+import { Card, CardDescription, CardPicture, CardPosition } from "@/components/Card/Card";
+
 export const UmlRandom = () => {
-    // const [dataUml] = useState(umlData);
+
     const [resultRandom, setResultRandom] = useState([]);
 
     const randomUml = () => {
@@ -19,18 +21,21 @@ export const UmlRandom = () => {
 
     if (resultRandom)
         return (
-            <div className="uml_random_contaner">
-                <Card>
-                    <CardPicture src={resultRandom.picture} alt="picture uml" />
-                    <CardDescription> {resultRandom.description}</CardDescription>
-                </Card>
-                <Button onClick={() => randomUml()}> Random </Button>
-
-            </div>
+            <>
+                <div className="uml_random_container">
+                    <Card>
+                        <CardPosition center="uml">
+                            <CardPicture src={resultRandom.picture} alt="picture uml" />
+                            <CardDescription> Le {resultRandom.name} </CardDescription>
+                        </CardPosition>
+                    </Card>
+                </div>
+                <Button variant="random" onClick={() => randomUml()}> Random </Button>
+            </>
         )
     return (
 
-        <div>   Loadding...
+        <div>   Loading...
 
         </div>
     )
