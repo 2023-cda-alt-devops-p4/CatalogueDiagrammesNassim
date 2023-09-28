75215a4d5d86908b15e54f3a922e2bdf1f35e6de
Deflandrenassim <nassim.def@gmail.com>
2023-09-28 15:01:01 +0200

    feat: add data doc  merise

--- uml/src/pages/merise.js
+++ uml/src/pages/merise.js
@@ -28,9 +28,8 @@ const merise = () => {
 
                 <div className="merise_learn">
                     <div className="merises"> La méthode MERISE date de 1978-1979, et fait suite à une consultation nationale lancée en 1977 par le ministère de l'Industrie dans le but de choisir des
-                        sociétés de conseil en informatique afin de définir une méthode de conception de systèmes d'information.
-                        Les deux principales sociétés ayant mis au point cette méthode sont le CTI (Centre Technique d'Informatique)
-                        chargé de gérer le projet, et le CETE (Centre d'Etudes Techniques de l'Equipement) implanté à Aix-en-Provence.
+                        sociétés de conseil en informatique afin de définir une méthode de conception de systèmes d'information. C'est une methode Francaise
+                        Merise est une  méthode qui a pour but de réaliser une application informatique permettant d'automatiser les tâches et les activités d'une entreprise.
                     </div>
                     <div className="merise_picture">
                         <Image src={ImgMerise} height={250} width={500} alt="merise" />
